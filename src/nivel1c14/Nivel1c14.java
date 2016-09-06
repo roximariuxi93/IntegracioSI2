a89f9a581779e92ba9b3dc6809fad0f0c940fc09
DESKTOP-RUQICM2\Usuario <irving_69@live.com>
2016-09-05 20:21:54 -0500

    tercer cambio

--- src/nivel1c14/Nivel1c14.java
+++ src/nivel1c14/Nivel1c14.java
@@ -21,7 +21,7 @@ public class Nivel1c14 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("ttravis integracion");
+        System.out.println("travis integracion");
         int [] vector;
         int tamaño=12,i=0;
         float acumula=0,promedio=0;
